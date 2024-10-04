2533c46a51f9ea7d1c25a86a8347e424fa5408ce
MorI3456 <caseycs@mail.uc.edu>
2024-10-04 14:06:12 -0400

    scanner change

--- out/production/Lab_6_Blocking_Input/HighorLow.java
+++ out/production/Lab_6_Blocking_Input/HighorLow.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class HighorLow {
     public static void main(String[] args) {
         Random generator = new Random();
-        Scanner in = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         int randomNumber = generator.nextInt(10) + 1;
         int guess = 0;
         boolean validInput;
@@ -12,16 +12,16 @@ public class HighorLow {
         // Get the guess
         do {
             System.out.print("Guess the number between 1 and 10: ");
-            validInput = in.hasNextInt();
+            validInput = scanner.hasNextInt();
             if (validInput) {
-                guess = in.nextInt();
+                guess = scanner.nextInt();
                 if (guess < 1 || guess > 10) {
                     System.out.println("Please enter a number between 1 and 10.");
                     validInput = false;
                 }
             } else {
                 System.out.println("Invalid input. Please enter a valid number.");
-                in.next();  // Clear invalid input
+                scanner.next();  // Clear invalid input
             }
         } while (!validInput);
 
